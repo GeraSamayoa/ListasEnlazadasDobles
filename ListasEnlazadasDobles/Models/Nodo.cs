946eeed7821aa9dc83183f2bab36446dbf92947f
GerardoSamayoa <gerardo.samayoa@noduu.com>
2024-04-07 23:14:53 -0600

    Agregar archivos de proyecto.

---
+++ ListasEnlazadasDobles/Models/Nodo.cs
@@ -0,0 +1,19 @@
+﻿namespace ListasEnlazadasDobles.Models
+{
+    public class Nodo
+    {
+        public Nodo? LigaAnterior { get; set; }
+
+        public string Informacion { get; set; }
+
+        public Nodo? LigaSiguiente { get; set; }
+
+        public Nodo(string informacion)
+        {
+            LigaAnterior = null;
+            Informacion = informacion;
+            LigaSiguiente = null;
+        }
+
+    }
+}
---
+++ ListasEnlazadasDobles/Services/ListaEnlazadaDoble.cs
@@ -0,0 +1,21 @@
+﻿using ListasEnlazadasDobles.Models;
+
+namespace ListasEnlazadasDobles.Services
+{
+    public class ListaEnlazadaDoble
+    {
+        public Nodo? PrimerNodo { get; set; }
+        public Nodo? UltimoNodo { get; set; }
+
+        public Nodo? NodoActual { get; set; }
+        public ListaEnlazadaDoble()
+        {
+            PrimerNodo = null;
+            UltimoNodo = null;
+            NodoActual = null;
+        }
+
+        public bool IsEmpty => PrimerNodo == null;
+
+    }
+}
