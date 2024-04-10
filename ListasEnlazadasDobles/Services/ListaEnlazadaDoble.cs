9e3534eb684a1cb35ae1351a5ed6c48f846ef7f8
JBAtres123 <Jbatres2r@miumg.edu.gt>
2024-04-09 20:43:56 -0600

    primer codigo JB

--- ListasEnlazadasDobles/Services/ListaEnlazadaDoble.cs
+++ ListasEnlazadasDobles/Services/ListaEnlazadaDoble.cs
@@ -6,6 +6,7 @@ namespace ListasEnlazadasDobles.Services
     {
         public Nodo? PrimerNodo { get; set; }
         public Nodo? UltimoNodo { get; set; }
+        public int Nodos { get; set; } 
 
         public Nodo? NodoActual { get; set; }
         public ListaEnlazadaDoble()
@@ -13,20 +14,161 @@ namespace ListasEnlazadasDobles.Services
             PrimerNodo = null;
             UltimoNodo = null;
             NodoActual = null;
+            Nodos = 0;
         }
-
         public bool IsEmpty => PrimerNodo == null;
 
-        public Nodo Siguiente()
+
+        public bool EsListaVacia() 
+        {
+            return PrimerNodo == null;
+        }
+
+        public Nodo? Siguiente()
         {
             NodoActual = NodoActual.LigaSiguiente ?? UltimoNodo;
             return NodoActual;
         }
 
-        public Nodo Anterior()
+        public Nodo? Anterior()
         {
             NodoActual = NodoActual.LigaAnterior ?? PrimerNodo;
             return NodoActual;
+        }
+
+
+
+        public string AgregarAlInicio(string dato) 
+        {
+            Nodo nuevoNodo = new Nodo(dato);
+            if (EsListaVacia())
+            {
+                PrimerNodo = UltimoNodo = nuevoNodo;
+            }
+            else
+            {
+                nuevoNodo.LigaSiguiente = PrimerNodo;
+                PrimerNodo.LigaAnterior = nuevoNodo;
+                PrimerNodo = nuevoNodo;
+            }
+            Nodos++;
+            return "nodo insertado";
+
+        }
+        public string InsertarDespuesDePosicionX(int posicion, string dato)
+        {
+            Nodo nuevoNodo = new Nodo(dato);
+
+            if (posicion <= 0)
+            {
+                return "La posición debe ser un número positivo mayor que cero.";
+            }
+
+            Nodo? nodoActual = PrimerNodo;
+            int contador = 1;
+
+            while (contador < posicion && nodoActual != null)
+            {
+                nodoActual = nodoActual.LigaSiguiente;
+                contador++;
+            }
+
+            if (nodoActual == null)
+            {
+                return "La posición especificada no existe en la lista.";
+            }
+
+            nuevoNodo.LigaSiguiente = nodoActual.LigaSiguiente;
+            if (nodoActual.LigaSiguiente != null)
+            {
+                nodoActual.LigaSiguiente.LigaAnterior = nuevoNodo;
+            }
+            nuevoNodo.LigaAnterior = nodoActual;
+            nodoActual.LigaSiguiente = nuevoNodo;
+
+            if (nodoActual == UltimoNodo)
+            {
+                UltimoNodo = nuevoNodo;
+            }
+
+            return "Nodo insertado después de la posición especificada.";
+        }
+
+        public string QuitarAlFinal()
+        {
+            if (EsListaVacia())
+            {
+                return "No Existen Elementos";
+            }
+            else if (PrimerNodo == UltimoNodo)
+            {
+                PrimerNodo = UltimoNodo = null;
+            }
+            else
+            {
+                Nodo? nodoEliminar;
+                nodoEliminar = UltimoNodo;
+                UltimoNodo = UltimoNodo.LigaAnterior;
+                UltimoNodo.LigaSiguiente = null;
+
+                nodoEliminar = null;
+            }
+            Nodos--;
+            return "nodo eliminado";
+        }
+        public string EliminarNodoConDatoX(object datoX)
+        {
+          
+            if (EsListaVacia())
+            {
+                return "La lista está vacía.";
+            }
+
+            Nodo nodoActual = PrimerNodo;
+            Nodo nodoAnterior = null;
+
+            
+            while (nodoActual != null && !nodoActual.Informacion.Equals(datoX))
+            {
+                nodoAnterior = nodoActual;
+                nodoActual = nodoActual.LigaSiguiente;
+            }
+
+        
+            if (nodoActual != null)
+            {
+           
+                if (PrimerNodo == UltimoNodo)
+                {
+                    PrimerNodo = UltimoNodo = null;
+                }
+                else
+                {
+                    
+                    if (nodoAnterior != null)
+                    {
+                        
+                        Nodo nodoTemporal = nodoAnterior;
+
+                      
+                        nodoAnterior.LigaSiguiente = nodoActual.LigaSiguiente;
+
+                   
+                        nodoActual.LigaAnterior = null;
+
+                      
+                        nodoTemporal = null;
+                    }
+                }
+
+                
+                return "Nodo con el dato especificado eliminado.";
+            }
+            else
+            {
+             
+                return "El dato especificado no se encontró en la lista.";
+            }
         }
 
     }
