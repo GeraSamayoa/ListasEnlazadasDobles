7fade1e59d0e90d678efd50840921bb5c93eb5d6
Sofia <asofimp@gmail.com>
2024-04-12 00:34:33 -0600

    Inicio

--- ListasEnlazadasDobles/Services/ListaEnlazadaDoble.cs
+++ ListasEnlazadasDobles/Services/ListaEnlazadaDoble.cs
@@ -29,5 +29,58 @@ namespace ListasEnlazadasDobles.Services
             return NodoActual;
         }
 
+        public string AgregarAlInicio(string dato)
+        {
+            Nodo? nuevoNodo = new Nodo(dato);
+
+            if (IsEmpty)
+            {
+                PrimerNodo = UltimoNodo = nuevoNodo;
+            }
+            else
+            {
+                nuevoNodo.LigaSiguiente = PrimerNodo;
+                PrimerNodo.LigaAnterior = nuevoNodo;
+                PrimerNodo = nuevoNodo;
+
+            }
+
+            return "Nodo insertado con exito!";
+        }
+
+        public string EliminarUnDatoX(string dato)
+        {
+            Nodo? nodoActual;
+            Nodo? nodoAuxiliarSiguiente;
+            Nodo? nodoAuxiliarAnterior;
+
+            if (IsEmpty){ return "La lista esta vacia";}
+
+            nodoActual = PrimerNodo;
+
+            while (nodoActual != null && nodoActual.Informacion == dato )
+            {
+                nodoActual = nodoActual.LigaSiguiente;
+            }
+
+            if(nodoActual == null)
+            {
+                return "Dato no encontrado";
+
+            }else if (nodoActual.Informacion == dato)
+            {
+                nodoAuxiliarAnterior = nodoActual.LigaAnterior;
+                nodoAuxiliarSiguiente = nodoActual.LigaSiguiente;
+
+                nodoActual = null;
+                nodoAuxiliarAnterior.LigaSiguiente = nodoAuxiliarSiguiente;
+                nodoAuxiliarSiguiente.LigaAnterior = nodoAuxiliarAnterior;
+
+            }
+
+            return "Nodo eliminado con exito";
+
+        }
+
     }
 }
